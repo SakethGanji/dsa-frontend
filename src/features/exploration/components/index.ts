2e9321d65223f952453786eb234215e8a1895cc1
Saketh Ganji <bsg200001@utdallas.edu>
2025-06-19 16:44:04 -0500

    remove

--- src/features/exploration/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { ExplorationView } from './ExplorationView';
-export { DatasetSelectionStep } from './DatasetSelectionStep';
-export { VersionSelectionStep } from './VersionSelectionStep';
-export { DataPreviewStep } from './DataPreviewStep';
-export { AnalysisSelectionStep } from './AnalysisSelectionStep';
-export { AnalysisResultsStep } from './AnalysisResultsStep';
--- src/features/exploration/hooks/index.ts
+++
@@ -1,4 +0,0 @@
-export { useExplorationState } from './useExplorationState';
-
-// Re-export exploration-related hooks from the old location
-export { useExploreDataset, createProfileRequest } from '@/hooks/use-exploration-query';
